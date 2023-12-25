512d0d2b4c8889aaf21b51a733b5669f26cb2fa1
PurplePower <60787289+PurplePower@users.noreply.github.com>
2023-12-25 20:06:23 +0800

    Update generate_bitstream.tcl
    fixed tiny bug

--- lab1/vivado/z710/generate_bitstream.tcl
+++ lab1/vivado/z710/generate_bitstream.tcl
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-source open_project-v2020.tcl
+source open_project.tcl
 
 while 1 {
     if { [catch {launch_runs synth_1 -jobs 4 } ] } {
